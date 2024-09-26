b2e2aefcfc12ce43020545942573e9e5d69c735a
unknown <mauryamanish5122000@gmai.com>
2024-09-26 18:39:41 +0530

    24/09/2024

--- api/urls.py
+++ api/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('send_message/', send_message, name='send_message'),
 
     # re_path(r'ws/chat/(?P<user1>\w+)/(?P<user2>\w+)/$', consumers.ChatConsumer.as_asgi(), name='ws_chat'),
-    # path('ws/chat/', consumers.SimpleChatConsumer.as_asgi()),
+    path('ws/chat/', consumers.SimpleChatConsumer.as_asgi()),
 
 
     
